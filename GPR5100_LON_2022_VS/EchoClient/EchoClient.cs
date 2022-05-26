e0ad96c3ad8adbeaebd8d2046da4356682c20132
NeoCambion <100194uk@saeinstitute.edu>
2022-05-26 17:05:15 +0100

    Change send method to broadcast

--- GPR5100_LON_2022_VS/EchoClient/EchoClient.cs
+++ GPR5100_LON_2022_VS/EchoClient/EchoClient.cs
@@ -9,15 +9,16 @@ namespace EchoClient
         static void Main(string[] args)
         {
             // Create remote endpoint
-            Console.WriteLine("Please enter target server IP address...");
-            string ipString = Console.ReadLine();
-            IPAddress remoteAddr = IPAddress.Parse(ipString);
+            // Console.WriteLine("Please enter target server IP address...\n(Use 255.255.255.255 to broadcast)");
+            // string ipString = Console.ReadLine();
+            IPAddress remoteAddr = IPAddress.Broadcast;
 
             Console.WriteLine("\nPlease enter target server socket...");
             int serverSocketInt = Int32.Parse(Console.ReadLine());
             IPEndPoint destinationEndPoint = new IPEndPoint(remoteAddr, serverSocketInt);
 
             Socket skt = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            skt.EnableBroadcast = true;
 
             bool endProcess = false;
             while (!endProcess)
