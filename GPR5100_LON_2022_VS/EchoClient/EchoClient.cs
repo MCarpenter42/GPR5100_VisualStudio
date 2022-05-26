d9e2300be6dc01210af5538e40d7fb7cf37b8668
NeoCambion <100194uk@saeinstitute.edu>
2022-05-26 16:57:25 +0100

    Initial file commit

---
+++ GPR5100_LON_2022_VS/EchoClient/EchoClient.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace EchoClient
+{
+    internal class EchoClient
+    {
+        static void Main(string[] args)
+        {
+            // Create remote endpoint
+            Console.WriteLine("Please enter target server IP address...");
+            string ipString = Console.ReadLine();
+            IPAddress remoteAddr = IPAddress.Parse(ipString);
+
+            Console.WriteLine("\nPlease enter target server socket...");
+            int serverSocketInt = Int32.Parse(Console.ReadLine());
+            IPEndPoint destinationEndPoint = new IPEndPoint(remoteAddr, serverSocketInt);
+
+            Socket skt = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            bool endProcess = false;
+            while (!endProcess)
+            {
+                Console.WriteLine("\n" + "Enter text to send to the server...");
+                string text = Console.ReadLine();
+
+                if (text == "exit")
+                {
+                    endProcess = true;
+                }
+                else if (text != null)
+                {
+                    byte[] sendData = System.Text.Encoding.ASCII.GetBytes(text);
+                    skt.SendTo(sendData, destinationEndPoint);
+                    Console.WriteLine(" > Sending...");
+                }
+                else
+                {
+                    Console.WriteLine(" > Error: no text present");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
+
+// 10.44.51.95
+// 55555
---
+++ GPR5100_LON_2022_VS/EchoServer/EchoServer.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace EchoServer
+{
+    class EchoServer
+    {
+        static void Main(string[] args)
+        {
+            // "IPAddress.Any" is equivalent to "IPAddress.Parse("0.0.0.0")"
+            IPAddress addr = IPAddress.Any;
+
+            // 55555 is not a reserved port, and so is typically available
+            int port = 55555;
+            IPEndPoint endPoint = new IPEndPoint(addr, port);
+
+            Socket skt = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            skt.Bind(endPoint);
+
+            Console.WriteLine("Opening socket, echoing from " + addr.ToString() + ":" + endPoint.Port);
+
+            byte[] buffer = new byte[1024];
+
+            while (true)
+            {
+                int byteCount = skt.Receive(buffer);
+                string text = System.Text.Encoding.ASCII.GetString(buffer, 0, byteCount);
+                Console.WriteLine(text);
+            }
+        }
+
+        static void LookupLoop()
+        {
+            bool endProcess = false;
+            bool firstRun = true;
+            while (!endProcess)
+            {
+                if (firstRun)
+                {
+                    firstRun = false;
+                    Console.WriteLine("< Enter \"exit\" instead of a hostname to terminate the process >");
+                }
+                endProcess = DNSLookup();
+                Console.WriteLine("");
+            }
+        }
+
+        // DNS service example
+        static bool DNSLookup()
+        {
+            Console.WriteLine("Please enter a hostname...");
+            string hostname;
+            while (true)
+            {
+                hostname = Console.ReadLine();
+                if (hostname != null)
+                {
+                    break;
+                }
+            }
+
+            if (hostname == "exit")
+            {
+                return true;
+            }
+
+            Console.WriteLine("\n" + hostname + " resolves to:");
+            IPAddress[] addresses = Dns.GetHostAddresses(hostname);
+            foreach (IPAddress addr in addresses)
+            {
+                Console.WriteLine("- " + addr.ToString());
+            }
+
+            return false;
+        }
+    }
+}
